74ccfc528da81a50956fee9b6a5edad845026221
jpils <pilsj00@gmail.com>
2026-05-23 19:02:01 +0200

    flakes

---
+++ rust_python/flake.nix
@@ -0,0 +1,65 @@
+{
+	description = "rust + python devenv";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+		flake-utils.url = "github:numtide/flake-utils";
+
+		fenix = {
+			url = "github:nix-community/fenix";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		crane.url = "github:ipetkov/crane";
+	};
+
+	outputs = { self, nixpkgs, flake-utils, fenix, crane, ... }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs { inherit system; };
+
+				tc = fenix.packages.${system}.stable;
+				toolchain = tc.withComponents [
+					"cargo"
+					"clippy"
+					"rust-src"
+					"rustc"
+					"rustfmt"
+					"rust-analyzer"
+				];
+
+				craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+				src = craneLib.cleanCargoSource ./.;
+				commonArgs = {
+					inherit src;
+					strictDeps = true;
+				};
+
+				cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+				crate = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+
+			in {
+				devShells.default = pkgs.mkShell {
+					packages = [
+						toolchain
+						fenix.packages.${system}.rust-analyzer
+						pkgs.bacon
+
+						pkgs.uv
+					];
+
+					RUST_SRC_PATH = "${tc.rust-src}/lib/rustlib/src/rust/library";
+					
+					LIBTORCH_USE_PYTORCH = "1";
+
+					shellHook = ''
+						echo "Syncing python virtual env with uv ..."
+						uv sync
+						
+						export PATH="$PWD/.venv/bin:$PATH"
+					'';
+				};
+				packages.default = crate;
+			});
+}
