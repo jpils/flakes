a1100fdbc2a2dc259e5d5b77f8e98c1404fabac9
jpils <pilsj00@gmail.com>
2026-05-25 10:15:24 +0200

    python flake

---
+++ python/flake.nix
@@ -0,0 +1,28 @@
+{
+	description = "python devenv with uv";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils, ... }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = import nixpkgs { inherit system; };
+			in {
+				devShells.default = pkgs.mkShell {
+					packages = [
+						pkgs.uv
+					];
+
+					shellHook = ''
+						echo "Syncing python virtual env with uv ..."
+						uv sync
+						
+						export PATH="$PWD/.venv/bin:$PATH"
+					'';
+				};
+			});
+}
